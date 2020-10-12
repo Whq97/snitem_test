de405e7b65d13265b3fb0c16c22872172708f35e
Whq97 <1042775965@qq.com>
2020-10-12 16:23:19 +0800

    列表页分页 排序第一波

---
+++ src/script/list_module.js
@@ -0,0 +1,234 @@
+define([], function() {
+    return {
+        render: ! function() { //渲染
+            let list = $('.product-list .general');
+            let array_default = []; //排序前的li数组
+            let array = []; //排序中的数组
+            //冒泡排序，比较相邻的两个数字。
+            let prev = null; //前一个商品价格
+            let next = null; //后一个商品价格
+            $.ajax({
+                url: 'http://192.168.11.7/js/suningitem_test/php/listdata.php',
+                dataType: 'json'
+            }).done(function(data) {
+                //进行渲染结构代码。
+                let strhtml = '';
+                $.each(data, function(index, value) {
+                    console.log(value);
+                    strhtml += `
+                    <li>
+                    <div class="item-bg">
+                        <div class="product-box">
+                            <div class="res-img">
+                                <div class="img-block">
+                                    <a href="#">
+                                        <img class="lazy" data-original="${value.url}" alt="">
+                                    </a>
+                                </div>
+                                <div class="focus-box ">
+                                    <div class="focus-img">
+                                        <dl>
+                                            <dd>
+                                                <a href="javascript:;">
+                                                    <img class="lazy" data-original="${value.url}" alt="">
+                                                </a>
+                                            </dd>
+                                        </dl>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="res-info">
+                                <div class="price-box">
+                                    <span>
+                                        <i>¥</i><em>${parseInt(value.price)}</em><i>.00</i>
+                                        <i class="price-bybt"></i>
+                                    </span>
+                                </div>
+                                <div class="title-selling-point">
+                                    <a href="#">${value.title}</a>
+                                </div>
+                                <div class="info-config">
+                                    <em> 128GB <i>|</i>6.1英寸</em>
+                                </div>
+                                <div class="evaluate-old">
+                                    <a href="#" class="a1">
+                                        <i>${value.sailnumber}+</i>评价
+                                    </a>
+                                    <a href="#" class="a2">
+                                        去淘二手
+                                    </a>
+                                </div>
+                                <div class="store-stock">
+                                    <p>苏宁自营</p>
+                                </div>
+                                <div class="sales-label">
+                                    <span>${value.activity}</span>
+                                </div>
+                            </div>
+                            <div class="res-opt">
+                                <a href="javascript:;" class="duibi">
+                                    <i></i> 对比
+                                </a>
+                                <a href="javascript:;" class="guanzhu">
+                                    <i></i> 关注
+                                </a>
+                                <a href="javascript:;" class="gouwuc">
+                                    <i></i> 购物车
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                `;
+                });
+                list.html(strhtml);
+                $("img.lazy").lazyload({
+                    effect: "fadeIn" //图片显示方式
+                });
+                $('.product-list .general li').each(function(index, element) {
+                    array[index] = $(this);
+                    array_default[index] = $(this);
+                });
+                //2.分页思路:根据传输的页码，后端返回对应的接口数据，渲染出来。
+
+            });
+            $('#page').pagination({
+                pageCount: 5, //总的页数
+                jump: true, //是否开启跳转到指定的页数，布尔值。
+                prevContent: '上一页', //将图标改成上一页下一页。
+                nextContent: '下一页',
+                callback: function(api) {
+                    console.log(api.getCurrent()); //获取当前的点击的页码。
+                    $.ajax({
+                        url: 'http://192.168.11.7/js/suningitem_test/php/listdata.php',
+                        data: {
+                            page: api.getCurrent() //传输数据
+                        },
+                        dataType: 'json'
+                    }).done(function(data) {
+                        let strhtml = '';
+                        $.each(data, function(index, value) {
+                            strhtml += `
+                                <li>
+                                <div class="item-bg">
+                                    <div class="product-box">
+                                        <div class="res-img">
+                                            <div class="img-block">
+                                                <a href="#">
+                                                    <img class="lazy" data-original="${value.url}" alt="">
+                                                </a>
+                                            </div>
+                                            <div class="focus-box ">
+                                                <div class="focus-img">
+                                                    <dl>
+                                                        <dd>
+                                                            <a href="javascript:;">
+                                                                <img class="lazy" data-original="${value.url}" alt="">
+                                                            </a>
+                                                        </dd>
+                                                    </dl>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="res-info">
+                                            <div class="price-box">
+                                                <span>
+                                                    <i>¥</i><em>${parseInt(value.price)}</em><i>.00</i>
+                                                    <i class="price-bybt"></i>
+                                                </span>
+                                            </div>
+                                            <div class="title-selling-point">
+                                                <a href="#">${value.title}</a>
+                                            </div>
+                                            <div class="info-config">
+                                                <em> 128GB <i>|</i>6.1英寸</em>
+                                            </div>
+                                            <div class="evaluate-old">
+                                                <a href="#" class="a1">
+                                                    <i>${value.sailnumber}+</i>评价
+                                                </a>
+                                                <a href="#" class="a2">
+                                                    去淘二手
+                                                </a>
+                                            </div>
+                                            <div class="store-stock">
+                                                <p>苏宁自营</p>
+                                            </div>
+                                            <div class="sales-label">
+                                                <span>${value.activity}</span>
+                                            </div>
+                                        </div>
+                                        <div class="res-opt">
+                                            <a href="javascript:;" class="duibi">
+                                                <i></i> 对比
+                                            </a>
+                                            <a href="javascript:;" class="guanzhu">
+                                                <i></i> 关注
+                                            </a>
+                                            <a href="javascript:;" class="gouwuc">
+                                                <i></i> 购物车
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            `;
+                        });
+                        list.html(strhtml);
+                        $("img.lazy").lazyload({
+                            effect: "fadeIn" //图片显示方式
+                        });
+
+
+                        //将页面的li元素加载到两个数组中
+                        $('.product-list .general li').each(function(index, element) {
+                            array[index] = $(this);
+                            array_default[index] = $(this);
+                        });
+                    });
+                }
+
+            });
+            //3.排序，排序前的数组都已经具有li元素
+            // 默认
+            $('.sort span').eq(0).on('click', function() {
+                $.each(array_default, function(index, value) {
+                    $('.product-list .general').append(value);
+                });
+                return;
+            });
+            // 升序
+            let flag = 1;
+            $('.sort span').eq(3).on('click', function() {
+                flag *= -1;
+                console.log(1);
+                for (let i = 0; i < array.length - 1; i++) {
+                    for (let j = 0; j < array.length - i - 1; j++) {
+                        prev = parseInt(array[j].find('.price-box span em').html()); //取上个价格
+                        next = parseInt(array[j + 1].find('.price-box span em').html()); //下一个的价格
+                        console.log(prev, next);
+                        //通过价格的判断，改变的是数组li的位置。
+                        if (flag > 0) {
+                            if (prev > next) {
+                                let temp = array[j];
+                                array[j] = array[j + 1];
+                                array[j + 1] = temp;
+                            }
+                        } else {
+                            if (prev < next) {
+                                let temp = array[j];
+                                array[j] = array[j + 1];
+                                array[j + 1] = temp;
+                            }
+                        }
+
+                    }
+                }
+
+                $.each(array, function(index, value) {
+                    $('.product-list .general').append(value);
+                });
+            });
+        }()
+    }
+})
--- src/script/main.js
+++ src/script/main.js
@@ -22,7 +22,8 @@ require.config({
     paths: { //模块路径,路径引入后缀名必须忽略，配置里面已经存在后缀名（扩展名）。
         'jquery': 'https://cdn.bootcdn.net/ajax/libs/jquery/1.12.4/jquery.min',
         'jq_cookie': 'https://cdn.bootcdn.net/ajax/libs/jquery-cookie/1.0/jquery.cookie.min',
-        'jq_lazyload': 'https://cdn.bootcdn.net/ajax/libs/jquery.lazyload/1.8.3/jquery.lazyload.min'
+        'jq_lazyload': 'https://cdn.bootcdn.net/ajax/libs/jquery.lazyload/1.8.3/jquery.lazyload.min',
+        'jq_pagination': 'http://192.168.11.7/js/suningitem_test/src/script/jquery.pagination'
     },
     shim: { //不支持AMD模块，支持AMD
         'jq_cookie': {
@@ -32,11 +33,15 @@ require.config({
         'jq_lazyload': {
             deps: ['jquery'], //模块的依赖
             exports: 'jq_lazyload'
+        },
+        'jq_pagination': {
+            deps: ['jquery'], //模块的依赖
+            exports: 'jq_pagination'
         }
     }
 });
 
 //加载模块
-require(['jquery', 'jq_cookie', 'jq_lazyload'], function() {
-    require(['index_module']);
+require(['jquery', 'jq_cookie', 'jq_lazyload', 'jq_pagination'], function() {
+    require(['index_module', 'list_module']);
 });
